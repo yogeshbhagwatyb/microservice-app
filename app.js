b969e7e9f7ebbf2f8ed4b314772acb7f61ba1eea
Bhagwat <12816@intelizign.com>
2025-03-28 13:22:18 +0530

    app.jsvis

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from Jenkins CI/CD pipeline on EC2!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`App listening at http://localhost:${port}`);
+// });
+
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Serve static files (if needed in future)
+app.use(express.static(__dirname));
+
+// Route to serve index.html
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins CI/CD pipeline on EC2!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App running at http://localhost:${port}`);
 });
